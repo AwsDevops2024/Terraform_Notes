78a8f0de8a02d28719d00cbe6b224243493077ae
Usharani Palem <palemusharani169@gmail.com>
2024-11-12 11:26:04 +0530

    Update Terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -2,7 +2,7 @@ provider "aws"{
   region = "us-east-1"
 }
 resource "aws_instance" "dev" {
-   ami = "ami-0ebfd941bbafe70c6"
+   ami = "ami-063d43db0594b521b"
    instance_type = "t2.micro"
    tags = {
      Name = "dev-ec2"
